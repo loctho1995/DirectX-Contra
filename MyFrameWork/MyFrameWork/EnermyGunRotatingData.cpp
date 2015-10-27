26d41dc473442657d126ac12bee423eb6a9f6c4d
BlackHole02 <hangnguyen081995@gmail.com>
2015-10-27 22:30:35 +0700

    EnermyGunRotating
    Thêm Sprites, thêm thuộc tính isDetroy

--- MyFrameWork/MyFrameWork/EnermyGunRotatingData.cpp
+++ MyFrameWork/MyFrameWork/EnermyGunRotatingData.cpp
@@ -5,6 +5,8 @@ EnermyGunRotatingData::EnermyGunRotatingData()
 	this->botName = "gunrotating";
 	this->isHittable = true;
 	this->isThrougable = false;
+	this->isDesTroyed = false;
+
 }
 
 EnermyGunRotatingData::~EnermyGunRotatingData()
