173e96b349df6949cdabc05be9502853eb241bb6
Thọ <buidinhloctho1995@gmail.com>
2015-12-02 13:43:36 +0700

    update staticweapon

--- MyFrameWork/MyFrameWork/ObjectStaticWeaponData.h
+++ MyFrameWork/MyFrameWork/ObjectStaticWeaponData.h
@@ -2,7 +2,7 @@
 #include "ObjectData.h"
 #include "Define.h"
 
-float const OBJECT_STATIC_WEAPON_RANGE_OPEN = 80.0f; //chuyen state khi player lai gan
+float const OBJECT_STATIC_WEAPON_RANGE_OPEN = 120.0f; //chuyen state khi player lai gan
 float const OBJECT_STATIC_WEAPON_RANGE_TURN = 15.0f; //bat lai khi cham vao camera
 
 class ObjectStaticWeaponData : public ObjectData
